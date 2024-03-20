fa694ed75814942a19793e7b7e0ae51493fc3c1f
IvanErmikhinn <ivanermihin@yandex.ru>
2024-03-20 19:06:11 +0300

    Поправили

--- src/main/java/PageObject/LocatorsPersonalCabinetPage.java
+++
@@ -1,8 +0,0 @@
-package PageObject;
-
-import org.openqa.selenium.By;
-
-public class LocatorsPersonalCabinetPage {
-    public static final By SAVE_BUTTON = By.xpath("//*[@id=\"root\"]/div/main/div/div/div/div/button[2]");
-    public static final By EXIT_BUTTON = By.xpath("/html/body/div/div/main/div/nav/ul/li[3]/button");
-}
